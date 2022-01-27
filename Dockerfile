53a7c42f34744248782e056a128498fffd98d295
Jesse Newland <jnewland@urcomputeringpal.com>
2022-01-27 14:53:03 +0000

    run ssh server

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@
 # supported by Google Cloud Run (https://cloud.google.com/run/docs/deploying#other-registries)
 FROM ghcr.io/cased/shell:v0.10.3
 COPY --from=ghcr.io/cased/jump:latest /bin/app /bin/jump
+COPY --from=ghcr.io/cased/ssh-oauth-handlers:pr-6 /bin/app /bin/ssh-oauth-handlers
 ENTRYPOINT [ "/bin/bash", "/entrypoint.sh" ]
 CMD []
 ADD entrypoint.sh jump.yml /
--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,12 @@ export CASED_SHELL_TLS=off
 export STORAGE_DIR=$HOME/.cased-shell
 : ${CASED_SHELL_LOG_LEVEL:="error"}
 
+let SSH_PORT=PORT+1 ;
+export CASED_SHELL_OAUTH_UPSTREAM=localhost:$SSH_PORT
+
+echo "starting ssh server"
+PORT=$SSH_PORT /bin/ssh-oauth-handlers cloudshell https://$CASED_SHELL_HOSTNAME bash -i &
+
 /bin/jump /jump.yml /tmp/jump.json &
 
 export CASED_SHELL_HOST_FILE=/tmp/jump.json
